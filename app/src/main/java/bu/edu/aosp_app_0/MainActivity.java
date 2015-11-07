0b3a6edaa7234ea4e9dc11f10028ea827ba8e830
Minnie Kim <minniek@bu.edu>
2015-11-07 18:39:09 -0500

    adding webview and floating action bar button to display process ID

--- app/src/main/java/bu/edu/aosp_app_0/MainActivity.java
+++ app/src/main/java/bu/edu/aosp_app_0/MainActivity.java
@@ -1,15 +1,17 @@
 package bu.edu.aosp_app_0;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView bu_wv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +20,13 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        bu_wv = (WebView) findViewById(R.id.bu_wv);
+
+        findViewById(R.id.getProcessId_fab).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "Process ID: " + android.os.Process.myPid(), Snackbar.LENGTH_INDEFINITE).show();
+                bu_wv.loadUrl("http://www.bu.edu");
             }
         });
     }
